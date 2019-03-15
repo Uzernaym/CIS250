7af4c603b6584dacb022232a217a2e210242a858
brandon.liu.72@gmail.com <brandon.liu.72@gmail.com>
2019-03-15 10:34:13 -0700

    some files

---
+++ MidtermBrandonLiuWhole/MidtermBrandonLiuWhole/MidtermBrandonLiuWhole.cpp
@@ -0,0 +1,52 @@
+//Brandon Liu - 3/14/2019 - CIS250
+
+//What is the sequence in which constructors and destructors are executed?
+//Constructors are run when an instance of the class is called. For example, Whole W1; 
+//Desctructors are run at the end of the scope of a class. In this case, that is when the main() function is complete.
+
+#include "pch.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Part {
+public:
+	Part() {
+		cout << "In default part constructor" << endl;
+	}
+	~Part() {
+
+	}
+};
+
+class Whole {
+private:
+	string wholeName;
+public:
+	Whole() {
+		wholeName = "None";
+		cout << "called default constructor " << wholeName << endl;
+	}
+	Whole(string name) {
+		wholeName = name;
+		cout  << "called parm constructor " << wholeName << endl;
+	}
+	string getName() {
+		return wholeName;
+	}
+	~Whole() {
+		cout << "called destructor " << wholeName << endl;
+	}
+};
+
+int main()
+{
+	Whole W1;
+	
+	Whole W2 = Whole("w2");
+
+	cout << "W1 wholeName = " << W1.getName() << endl;
+	cout << "W2 wholeName = " << W2.getName() << endl;
+	return 0;
+}

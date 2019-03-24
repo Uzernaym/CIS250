40b85383e8ba7e91d7b27184c664d11041c00bda
brandon.liu.72@gmail.com <brandon.liu.72@gmail.com>
2019-03-23 23:36:08 -0700

    ArrayPtr2

---
+++ Week10BrandonLiuMyArrayPtrArith2/Week10BrandonLiuMyArrayPtrArith2/Week10BrandonLiuMyArrayPtrArith2.cpp
@@ -0,0 +1,70 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+template <class T>
+class myArrayClass {
+private:
+	int arraySize;
+	T * ptrArray;
+public:
+	myArrayClass() {
+		arraySize = 0;
+		ptrArray = NULL;
+	}
+	myArrayClass(int inX) {
+		if (inX > 0) {
+			arraySize = inX;
+			ptrArray = new T[arraySize];
+		}
+
+	}
+	void setSize(int inX) {
+		if (ptrArray == NULL && inX > 0) {
+			arraySize = inX;
+			ptrArray = new T[arraySize];
+		}
+	}
+	void setAllValues() {
+		if (ptrArray != NULL) {
+			for (int i = 0; i < arraySize; i++) {
+				T input;
+				cout << "array[" << i << "] = ";
+				cin >> input;
+				ptrArray[i] = input;
+			}
+			cout << endl;
+		}
+	}
+	void printAll() {
+		if (ptrArray != NULL) {
+			for (int i = 0; i < arraySize; i++) {
+				cout << "arr[" << i << "] = " << ptrArray[i] << endl;
+			}
+		}
+	}
+};
+
+int main() {
+	cout << "Name: Brandon Liu - Program Name: MyArrayPtrArith1 - Date: 3/23/2019\n";
+
+	cout << "Array1" << endl;
+	myArrayClass<int> Array1;
+	Array1.setSize(6);
+	Array1.setAllValues();
+	Array1.printAll();
+	cout << endl << "Array2" << endl;
+
+	myArrayClass<double> Array2(7);
+	Array2.setAllValues();
+	Array2.printAll();
+	cout << endl << "Array3" << endl;
+
+	myArrayClass<long int> * ptrArray1 = new myArrayClass <long int>;
+	ptrArray1->setSize(7);
+	ptrArray1->setAllValues();
+	ptrArray1->printAll();
+
+	system("pause");
+	return 0;
+}

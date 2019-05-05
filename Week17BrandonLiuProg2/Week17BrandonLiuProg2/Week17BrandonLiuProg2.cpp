01f5a23a66079e67c8a47d5997bbf94f714aab52
brandon.liu.72@gmail.com <brandon.liu.72@gmail.com>
2019-05-05 16:48:38 -0700

    Prog2

--- Week17BrandonLiuProg2/Week17BrandonLiuProg2/Week17BrandonLiuProg2.cpp
+++ Week17BrandonLiuProg2/Week17BrandonLiuProg2/Week17BrandonLiuProg2.cpp
@@ -2,27 +2,38 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
 	cout << "Name: Brandon Liu - Program Name: Prog 2 - Date: 5/5/2019\n";
 
 	string aWord, aLine;
-	char aChar;
 	ifstream fin;
+	vector<string> data;
+	vector<int> age;
+	int maxNum = 0;
+	int minNum = 0;
 
 	fin.open("firstLastAge.txt");
 
 	if (!fin) {
 		exit(1);
 	}
-
 	while (!fin.eof()) {
 		getline(fin, aLine);
-		cout << aLine << endl;
+		data.push_back(aLine);
 	}
-
 	fin.close();
+	for (int i = 0; i < data.size(); i++) {
+		age.push_back(stoi(data[i].substr(data[i].length() - 2)));
+	}
+	for (int i = 0; i < data.size(); i++) {
+		if (age[i] > age[maxNum]) maxNum = i;
+		if (age[i] < age[minNum]) minNum = i;
+	}
+	cout << "Youngest Person = " << data[minNum] << endl;
+	cout << "Oldest Person = " << data[maxNum] << endl;
 
 	system("pause");
 	return 0;

1ddcb9461b6d6186239549036fcd926060eec672
brandon.liu.72@gmail.com <brandon.liu.72@gmail.com>
2019-05-06 14:49:53 -0700

    file reading is broke

--- FinalProjectMemoryMatchBrandon Liu/FinalProjectMemoryMatchBrandon Liu/FinalProjectMemoryMatchBrandon Liu.cpp
+++ FinalProjectMemoryMatchBrandon Liu/FinalProjectMemoryMatchBrandon Liu/FinalProjectMemoryMatchBrandon Liu.cpp
@@ -1,15 +1,55 @@
 // FinalProjectMemoryMatchBrandon Liu.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include "pch.h"
+#include <iostream>
+#include <fstream>
 #include <string>
-#include "MemoryMatchGame.h"
+#include <vector>
 
 using namespace std;
 
+class MemoryMatchGame {
+	
+public:
+	MemoryMatchGame() {
+		cout << "Memory Match game started" << endl;
+	}
+
+	void grabFile(string name, vector<string> data) {
+		ifstream fin;
+		string aLine, aWord;
+		fin.open(name + ".txt");
+
+		cout << name + ".txt";
+
+		if (!fin) {
+			cerr << "It's broke" << endl;
+			exit(1);
+		}
+		while (!fin.eof()) {
+			fin >> aWord;
+			getline(fin, aLine);
+			data.push_back(aWord);
+		}
+	}
+};
+
 int main()
 {
-    cout << "Hello World!\n";
-	MemoryMatchGame m1;
-	m1.printWords();
+	vector<string> food, states, animals;
+	string categories[3] = { "food", "states", "animals" };
 
+	MemoryMatchGame m1;
+	m1.grabFile(categories[0], food);
+	
+	
+	
+	for (int i = 0; i < food.size(); i++) {
+		cout << food[i] << endl;
+	}
+
+	system("pause");
+	return 0;
 }
+
+
